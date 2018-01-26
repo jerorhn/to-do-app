683221889cbb458cee7f2940d7b15377229f034e
Hannah Jeror <jerorhn@yahoo.com>
2018-01-26 15:00:21 -0500

    Chpt10- fix delete button

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ function onReady() {
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
+  //add delete button selector
+  const deleteButton = document.getElementById('deleteButton');
 
   addToDoForm.addEventListener('submit', () => {
-    if (newToDoText.value == 'delete') {
-      return;
-    }
-    else if (newToDoText.value != 'delete') {
       event.preventDefault();
 
       let title = newToDoText.value;
       let newLi = document.createElement('li');
       let checkbox = document.createElement('input');
+      //add class to checkbox to be able to select all checkboxes (creates an arr)
+      checkbox.className = 'boxes';
       checkbox.type = 'checkbox';
       //give the title var a use
       newLi.textContent = title;
@@ -22,7 +22,22 @@ function onReady() {
       toDoList.appendChild(newLi);
       //empty input
       newToDoText.value = '';
-    }
+  });
+
+  //add event listener to delete button when clicked
+  deleteButton.addEventListener('click', () => {
+    //prevent refreshing
+    event.preventDefault();
+
+    //select arr of all checkboxes
+    var listOfBoxes = document.getElementsByClassName('boxes');
+    //loop through all checkboxes (using their index)
+    for (var i = 0; i < listOfBoxes.length; i++) {
+      //if checked, delete li (parent) of said checkbox
+      if (listOfBoxes[i].checked) {
+        listOfBoxes[i].parentNode.remove('listOfBoxes[i].parentNode');
+      };
+    };
   });
 }
 
